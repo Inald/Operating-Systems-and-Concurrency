4c05beae8615506c08004c794a522f240a6f2ef2
Michael Mee <psympm@linode010.cs.nott.ac.uk>
2019-10-10 16:27:41 +0100

    misc

--- PartOne/task1a.c
+++ PartOne/task1a.c
@@ -6,12 +6,18 @@
 #include "linkedlist.h"
 
 // Implementation of the FCFS scheduling algorithm (by Michael and Inald)
+// OSC Coursework - Part One - Task 1a
 int main()
 {
   pid_t pid;
-  int pBurst, nBurst, response, turnAround;
+  int pBurst, nBurst, response, turnAround, i;
   double avgResponse, avgTurnAround;
-  
+  struct element* processList = (element*)malloc(sizeof(element)*NUMBER_OF_JOBS);
+  for(i = 0; i < NUMBER_OF_JOBS; i++)
+  {
+  }
+    
+  free(processList);
   printf("Process ID = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d\n", pid, pBurst, nBurst, response, turnAround);
   printf("Average response time = %f\n Average turn around time = %f\n", avgResponse, avgTurnAround);
   return 0;
