589321e312c836218ccbc171e0b7a54bacbab9a2
Michael Mee <psympm@linode172.cs.nott.ac.uk>
2019-10-10 11:14:07 +0100

    added libraries, variables and print statements

--- PartOne/task1a.c
+++ PartOne/task1a.c
@@ -1,6 +1,15 @@
+#include <sys/types.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main()
 {
+  pid_t pid;
+  int pBurst, nBurst, response, turnAround;
+  double avgResponse, avgTurnAround;
+  
+  printf("Process ID = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d\n", pid, pBurst, nBurst, response, turnAround);
+  printf("Average response time = %f\n Average turn around time = %f", avgResponse, avgTurnAround);
   return 0;
 }
