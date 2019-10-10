d29f9eba3568f2bae02aea8a6a6b1c2da21f1837
Michael Mee <psympm@linode010.cs.nott.ac.uk>
2019-10-10 17:38:58 +0100

    further additions - testing for linked list

--- PartOne/task1a.c
+++ PartOne/task1a.c
@@ -7,18 +7,31 @@
 
 // Implementation of the FCFS scheduling algorithm (by Michael and Inald)
 // OSC Coursework - Part One - Task 1a
+struct element** createProcesses(struct element** processList)
+{
+  int i;
+  struct process* newProcess;
+  printf("%x\n", &processList);
+  for(i = 0; i < NUMBER_OF_JOBS; i++)
+  {
+    newProcess = generateProcess();
+    printf("GENERATED PROCESS - ID: %x, Priority:%x\n", newProcess -> iProcessId, newProcess ->iPriority);
+    addLast(NULL, (struct element**)newProcess + sizeof(struct element**), (struct element**)newProcess + sizeof(struct element**));
+  }
+  printf("The id of the 5th process is %x\n", (processList->phead->iProcessId);
+  return processList;
+}
+
 int main()
 {
   pid_t pid;
   int pBurst, nBurst, response, turnAround, i;
   double avgResponse, avgTurnAround;
-  struct element* processList = (element*)malloc(sizeof(element)*NUMBER_OF_JOBS);
-  for(i = 0; i < NUMBER_OF_JOBS; i++)
-  {
-  }
-    
-  free(processList);
+  //Create a pointer that points to a pointer of elements
+  struct element** processList = createProcesses(processList);;
   printf("Process ID = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d\n", pid, pBurst, nBurst, response, turnAround);
-  printf("Average response time = %f\n Average turn around time = %f\n", avgResponse, avgTurnAround);
+  printf("Average response time = %f\nAverage turn around time = %f\n", avgResponse, avgTurnAround);
   return 0;
 }
+
+
