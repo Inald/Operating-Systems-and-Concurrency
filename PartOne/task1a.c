e924181bf0afa5c58876e477794ce3b689b9270d
Inald Gjoni <psyig@linode090.cs.nott.ac.uk>
2019-10-10 11:17:13 +0100

    \n changes

--- PartOne/task1a.c
+++ PartOne/task1a.c
@@ -10,6 +10,6 @@ int main()
   double avgResponse, avgTurnAround;
   
   printf("Process ID = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d\n", pid, pBurst, nBurst, response, turnAround);
-  printf("Average response time = %f\n Average turn around time = %f", avgResponse, avgTurnAround);
+  printf("Average response time = %f\n Average turn around time = %f\n", avgResponse, avgTurnAround);
   return 0;
 }
