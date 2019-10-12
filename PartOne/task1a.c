466c688c21227f2dd9d9f4722590d4d0c3713ffd
Michael Mee <psympm@linode048.cs.nott.ac.uk>
2019-10-12 17:17:14 +0100

    better structure

--- PartOne/task1a.c
+++ PartOne/task1a.c
@@ -8,7 +8,16 @@
 // Implementation of the FCFS scheduling algorithm (by Michael and Inald)
 // OSC Coursework - Part One - Task 1a
 
-int main()
+void algorithmFCFS(struct element **head, struct element **tail)
+{
+  int i;
+  //Remove in FCFS style - FIFO
+  for(i = 1; i <= NUMBER_OF_JOBS; i++)
+  {
+    removeFirst(head, tail);
+  }
+}
+void createJobs()
 {
   pid_t pid;
   int i, pBurst, nBurst, response, turnAround = 0, sumResponse = 0, sumTurnAround = 0;
@@ -20,27 +29,36 @@ int main()
   for(i = 1; i <= NUMBER_OF_JOBS; i++)
   {
     newProcess = generateProcess();
+    //pointers to process times
     createdPtr = &(newProcess -> oTimeCreated);
     finishPtr = &(newProcess -> oMostRecentTime);
-    addLast(newProcess, head, tail);
+    //add process to the linked list
     runNonPreemptiveJob(newProcess, createdPtr, finishPtr);
+    addLast(newProcess, head, tail);
+    //run the process
+    //set variables to process details
     pid = newProcess -> iProcessId;
     pBurst = newProcess ->iPreviousBurstTime;
     nBurst = newProcess ->iRemainingBurstTime;
+    //response time is previous turnAround time
     response = turnAround;
+    //turnAround is previous turnAround + previous burst time
     turnAround += (newProcess -> iPreviousBurstTime);
+    //calculate total sums of response time and turnaround times
     sumResponse += response;
     sumTurnAround += turnAround;
     printf("Process ID = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d\n", pid, pBurst, nBurst, response, turnAround);
     free(newProcess);
   }
+  //Apply FCFS algorithm to elements in linked list
+  algorithmFCFS(head, tail);
+  //Calculate and return averages for response time and turn around time
   printf("Average response time = %f\nAverage turn around time = %f\n", (double)sumResponse/(double)NUMBER_OF_JOBS, (double)sumTurnAround/(double)NUMBER_OF_JOBS);
-  //Remove in FCFS style - FIFO
-  for(i = 1; i <= NUMBER_OF_JOBS; i++)
-  {
-     removeFirst(head, tail);
-  }
-  return 0;
+}
+int main()
+{
+  createJobs();
+  return 0; 
 }
 
 
