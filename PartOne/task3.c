0826c0b4b1ebf109250502b7b2cb398bdae2e74d
Michael Mee <psympm@linode079.cs.nott.ac.uk>
2019-10-31 10:15:00 +0000

    help me

--- PartOne/task3.c
+++ PartOne/task3.c
@@ -8,28 +8,24 @@
 #include "linkedlist.h"
 
 //Semaphores for sync, delaying consumer and counting number of jobs
-sem_t sSync, sDelayProducer, sFull;
-int produced = 0, consumed = 0, counter = 0, errors = 0;
+sem_t sSync, sDelayProducer, sFreeElements;
+int produced = 0, consumed = 0;
 //Create head and tail pointers to pointers for the linked list
 struct element *ptrH = NULL, *ptrT = NULL;
 struct element **head = &ptrH, **tail = &ptrT;
 
 void visualisation(int sender, pid_t ID)
 {
-    //Sender is >0 for a producer or 0 for a consumer
+    int count;
     struct element *elem;
-    if(counter > MAX_BUFFER_SIZE)
-    {
-        printf("ERROR\n\n");
-        errors++;
-    }
+    //If 0, sender is producer else consumer
     if(sender > 0)
     {
-        printf("Producer %d, Produced = %d Consumed = %d: Count = %d ", ID, produced, consumed, counter);
+        printf("Producer %d Produced = %d Consumed = %d: ", ID, produced, consumed);
     }
     else
     {
-        printf("Consumer %d, Produced = %d Consumed = %d: Count = %d ", ID, produced, consumed, counter);
+        printf("Consumer %d Produced = %d Consumed = %d: ", ID, produced, consumed);
     }
     //Print out element data of each element in the list
     if(*head)
@@ -44,23 +40,23 @@ void visualisation(int sender, pid_t ID)
 
 void * consumerFunc()
 {
-    int count;
+    int count ; 
     pid_t cID = 1;
-    sem_post(&sDelayProducer);
+    sem_wait(&sDelayProducer);
     while(consumed < MAX_NUMBER_OF_JOBS)
     {
         sem_wait(&sSync);
-        if(counter > 0)
+        sem_getvalue(&sFreeElements, &count);
+        printf("sFreeElements = %d\n", count);
+        removeFirst(head, tail);
+        consumed++;
+        visualisation(0, cID);
+        if(count + 1 >= MAX_BUFFER_SIZE && consumed < MAX_NUMBER_OF_JOBS)
         {
-            removeFirst(head, tail);
-            consumed++;
-            counter--;
-            visualisation(0, cID);
-        }
-        else
-        {
+            printf("AWAKEN PRODUCER\n");
             sem_post(&sDelayProducer);
         }
+        sem_post(&sFreeElements);
         sem_post(&sSync);
     }
 }
@@ -70,28 +66,33 @@ void * producerFunc()
 {
     int count;
     pid_t pID = 1;
+    sem_post(&sDelayProducer);
     while(produced < MAX_NUMBER_OF_JOBS)
     {
         sem_wait(&sSync);
+        sem_getvalue(&sFreeElements, &count);
+        printf("sFreeElements = %d\n", count);
         addLast((char *)'*', head, tail);
         produced++;
-        counter++;
         visualisation(1, pID);
-        sem_post(&sSync);
-        if(counter >= MAX_BUFFER_SIZE && produced < MAX_NUMBER_OF_JOBS)
+        if(count - 1 <= 0 && produced < MAX_NUMBER_OF_JOBS)
         {
+            printf("SLEEP PRODUCER\n");
             sem_wait(&sDelayProducer);
+            printf("THIS SHOULD NOT PRINT   \n");
         }
+        sem_wait(&sFreeElements);
+        sem_post(&sSync);
     }
 }
 
 int main(int argc, char **argv)
 {
     pthread_t consumer, producer;
-    int finalSync, finalDelayProducer, finalFull;
+    int finalSync, finalDelayProducer, finalElements;
     sem_init(&sSync, 0 , 1);
     sem_init(&sDelayProducer, 0 , 0);
-    sem_init(&sFull, 0, 0);
+    sem_init(&sFreeElements, 0, MAX_BUFFER_SIZE);
     pthread_create(&producer, NULL, producerFunc, NULL);
     pthread_create(&consumer, NULL, consumerFunc, NULL);
     pthread_join(producer, NULL);
@@ -99,7 +100,7 @@ int main(int argc, char **argv)
     //Final values of semapores
     sem_getvalue(&sSync, &finalSync);
     sem_getvalue(&sDelayProducer,&finalDelayProducer);
-    sem_getvalue(&sFull, &finalFull);
-    printf("sSync = %d, sDelayProducer = %d, finalFull = %d, Errors: %d\n", finalSync, finalDelayProducer, finalFull, errors);
+    sem_getvalue(&sFreeElements, &finalElements);
+    printf("sSync = %d, sDelayProducer = %d, sFreeElements = %d\n", finalSync, finalDelayProducer, finalElements);
     return 0;
 }
