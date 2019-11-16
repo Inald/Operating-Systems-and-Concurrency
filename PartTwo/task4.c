ae1327cbaaadbf0e0a630827c981993226d2ad92
Michael Mee <psympm@linode014.cs.nott.ac.uk>
2019-11-16 18:53:31 +0000

    no seg, but gotta ded

--- PartTwo/task4.c
+++ PartTwo/task4.c
@@ -58,30 +58,50 @@ void * consumerFunc(void *id)
     struct timeval start, end;
     while(consumed < NUMBER_OF_JOBS)
     {
-        sem_wait(&sSync);
         //If elements exist, can remove
         for(int i = 0; i < MAX_PRIORITY; i++)
         {
-            if(queueSizes[i] > 0)
+            sem_wait(&sSync);
+            printf("Consumer - synced correctly\n");
+            sem_getvalue(&queueSizes[i], &count);
+            printf("Consumer - got value of queueSizes[%d] as %d\n", i, count);
+            if(count < MAX_BUFFER_SIZE)
             {
-                firstProcess = (struct process *)removeFirst(priorityHeadArray[i], priorityTailArray[i]);
-                start = firstProcess -> oTimeCreated;
-                end = firstProcess -> oMostRecentTime;
-                runJob(firstProcess, &start, &end);
-                queueSizes[i]--;
-                //Wake up producer
-                if(consumed < NUMBER_OF_JOBS && queueSizes[i] <= 0)
-                {
-                    sem_post(&sDelayProducer);
-                }
-                consumed++;
-                firstProcess = processJob(cID, firstProcess, start, end);
-                if(firstProcess)
-                {
-                    addLast(firstProcess, priorityHeadArray[i], priorityTailArray[i]);
-                    queueSizes[i]++;
-                }
+                printf("Consumer - entered if statement with count as %d\n", count);
+                if(*priorityHeadArray[i])
+                {   
+                    printf("Consumer - head is not null\n");
+                    firstProcess = ((struct process *)removeFirst(priorityHeadArray[i], priorityTailArray[i]));
+                    printf("Consumer - removed process %d with priority %d correctly\n", firstProcess->iProcessId, firstProcess->iPriority);
+                    start = firstProcess -> oTimeCreated;
+                    end = firstProcess -> oMostRecentTime;
+                    printf("Consumer - assigned timevals correctly\n");
+                    runJob(firstProcess, &start, &end);
+                    printf("Consumer - run process correctly\n");
+                    firstProcess = processJob(cID, firstProcess, start, end);
+                    if(firstProcess)
+                    {
+                        printf("Consumer - firstprocess not null correctly\n");
+                        addLast(firstProcess, priorityHeadArray[i], priorityTailArray[i]);
+                        printf("Consumer - added process back correctly\n");
+                        sem_wait(&queueSizes[i]);
+                        printf("Consumer - waited queueSizes correctly\n");
+                        consumed--;
+                    }
+                    else
+                    {
+                        consumed++;
+                        sem_post(&queueSizes[i]);
+                        printf("Consumer - posted queueSizes correctly\n");
+                    }
+                }    
             }
+            else if(consumed < NUMBER_OF_JOBS && producerAwake == 0)
+            {
+                producerAwake = 1;
+                sem_post(&sDelayProducer);
+                printf("Consumer - post synced correctly\n");
+            }       
             sem_post(&sSync);
             
         }
@@ -91,28 +111,36 @@ void * consumerFunc(void *id)
 
 void * producerFunc(void *id)
 {
-    int count, pID = (*(int *)id);
+    int priority, pID = (*(int *)id), count;
     struct process *newProcess;
     sem_wait(&sDelayProducer);
     while(produced < NUMBER_OF_JOBS)
     {
         sem_wait(&sSync);
+        printf("Producer - synced correctly\n");
         newProcess = generateProcess();
-        for(int i = 0; i < MAX_PRIORITY; i++)
+        printf("Producer - generated process correctly\n");
+        priority = newProcess -> iPriority;
+        printf("Producer - assigned priority correctly\n");
+        sem_getvalue(&queueSizes[priority], &count);
+        printf("Producer, value of queueSizes[%d] = %d\n", priority, count);
+        if(count > 0)
         {
-            if(i == newProcess -> iPriority && produced < NUMBER_OF_JOBS)
-            {
-                if(queueSizes[i] > MAX_BUFFER_SIZE)
-                {
-                    sem_wait(&sDelayProducer);
-                }
-                addLast(newProcess, priorityHeadArray[newProcess->iPriority], priorityTailArray[newProcess->iPriority]);
-                printf("Producer 1, Process ID = %d, Priority = %d, Previous Burst Time = %d\n", newProcess->iProcessId, newProcess->iPriority, newProcess->iPreviousBurstTime);
-                queueSizes[newProcess->iPriority]++;
-                produced++;
-            }
-            sem_post(&sSync);
+            printf("Producer - entered loop with count as %d\n", count);
+            addLast(newProcess, priorityHeadArray[priority], priorityTailArray[priority]);
+            printf("Producer - added process %d to priority %d correctly\n", newProcess->iProcessId, priority);
+            produced++;
+            sem_wait(&queueSizes[priority]);
+            printf("Producer - waited queueSizes[%d] correctly\n", priority);
         }
+        else if(produced < NUMBER_OF_JOBS && producerAwake == 1)
+        {
+            producerAwake = 0;
+            sem_wait(&sDelayProducer);
+            printf("Producer - waited producer correctly\n");
+        }
+        sem_post(&sSync);
+        printf("Producer - posted synced correctly\n");
     }
 }
 int main(int argc, char **argv)
@@ -122,13 +150,10 @@ int main(int argc, char **argv)
     for(int i = 0; i < MAX_PRIORITY; i++)
     {
         priorityHeadArray[i] = priorityTailArray[i] = malloc(sizeof(struct element **));
+        sem_init(&queueSizes[i], 0, MAX_BUFFER_SIZE);
     }
     sem_init(&sSync, 0 , 1);
     sem_init(&sDelayProducer, 0 , 1);
-    for(int i = 0; i < MAX_PRIORITY; i++)
-    {
-        sem_init(&queueSizes[i], 0, MAX_BUFFER_SIZE);
-    }
     //Create producer and consumers
     for(int i = 0; i < NUMBER_OF_PRODUCERS; i++)
     {
@@ -138,6 +163,7 @@ int main(int argc, char **argv)
     {
         pthread_create(&consumer, NULL, consumerFunc, (void *)&i);
     }
+    printf("TEST -- all semaphores initialised correctly\n");
     pthread_join(producer, NULL);
     pthread_join(consumer, NULL);
     //Final values of semapores
