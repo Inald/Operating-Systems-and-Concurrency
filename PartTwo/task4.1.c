9342c3427056310c21c44c83c1e44208597ca397
Michael Mee <psympm@linode033.cs.nott.ac.uk>
2019-11-20 17:12:40 +0000

    working but incorrect ids??

--- PartTwo/task4.1.c
+++ PartTwo/task4.1.c
@@ -10,18 +10,27 @@
 //Semaphores for sync, delaying consumer
 //Counting semaphore represents number of free elements (number of jobs not filled)
 sem_t sSync, sEmpty, sFull;
-int produced = 0, consumed = 0, producerAwake = 0;
+int produced = 0, consumed = 0, dAverageResponseTime, dAverageTurnAroundTime;
 //Create head and tail pointers to pointers for the linked list
 // struct element *ptrH = NULL, *ptrT = NULL;
 // struct element **head = &ptrH, **tail = &ptrT;
 struct element *headArray[MAX_PRIORITY], *tailArray[MAX_PRIORITY];
+double calcAverage(int sum, int n)
+{
+  return ((double)sum/(double)n);
+}
 
+void printSems()
+{
+    int sema, semb;
+    sem_getvalue(&sFull, &sema);
+    sem_getvalue(&sEmpty, &semb);
+    printf("Full = %d, Empty = %d\n", sema, semb);
+}
 struct process * processJob(int iConsumerId, struct process * pProcess, struct timeval oStartTime, struct timeval oEndTime)
 {
 	int iResponseTime;
 	int iTurnAroundTime;
-	int dAverageResponseTime;
-	int dAverageTurnAroundTime;
 	if(pProcess->iPreviousBurstTime == pProcess->iInitialBurstTime && pProcess->iRemainingBurstTime > 0)
 	{
 		iResponseTime = getDifferenceInMilliSeconds(pProcess->oTimeCreated, oStartTime);	
@@ -58,10 +67,6 @@ struct process * processJob(int iConsumerId, struct process * pProcess, struct t
      struct timeval start, end;
      while(consumed < MAX_NUMBER_OF_JOBS)
      {
-        sem_getvalue(&sFull, &count);
-        if(count <= 3 && consumed >= MAX_NUMBER_OF_JOBS){
-            break;
-        }
         sem_wait(&sFull);
         sem_wait(&sSync);
         for(int i = 0; i < MAX_PRIORITY; i++){
@@ -71,10 +76,12 @@ struct process * processJob(int iConsumerId, struct process * pProcess, struct t
             }
         }
         firstProcess = removeFirst(&headArray[currentPriority], &tailArray[currentPriority]);
+        sem_post(&sSync);
         //check for values of time
         start = firstProcess -> oTimeCreated;
         end = firstProcess -> oMostRecentTime; 
         runJob(firstProcess, &start, &end);
+        sem_wait(&sSync);
         processRun = processJob(cID, firstProcess, start, end);
         if(processRun){
             addLast(processRun, &headArray[currentPriority], &tailArray[currentPriority]);
@@ -83,10 +90,7 @@ struct process * processJob(int iConsumerId, struct process * pProcess, struct t
             consumed++;
             sem_post(&sEmpty);
         }
-        //printf("consumer = %d\n", consumed);
-        sem_post(&sSync);
-        
-        
+        sem_post(&sSync);    
      }
      printf("Consumer finished = %d\n", cID);
 }
@@ -104,7 +108,6 @@ void * producerFunc()
         newProcess = generateProcess();
         addLast(newProcess, &headArray[newProcess -> iPriority], &tailArray[newProcess -> iPriority]);
         produced++;
-        //printf("produced = %d\n", produced);
         sem_post(&sSync);
         sem_post(&sFull);
         
@@ -113,25 +116,25 @@ void * producerFunc()
 
 int main(int argc, char **argv)
 {
-    pthread_t consumer, producer;
+    pthread_t consumer[NUMBER_OF_CONSUMERS], producer;
     int finalSync, finalEmpty, finalFull, id = 0;
     sem_init(&sSync, 0 , 1);
     sem_init(&sEmpty, 0 , MAX_BUFFER_SIZE);
     sem_init(&sFull, 0, 0);
-
-    pthread_create(&producer, NULL, producerFunc, &id);
+    pthread_create(&producer, NULL, producerFunc, NULL);
 
     for(int i = 0; i < NUMBER_OF_CONSUMERS; i++){
-         pthread_create(&consumer, NULL, consumerFunc, &id);
-         id++;
+        id++;
+        pthread_create(&consumer[i], NULL, consumerFunc, &id);
+        pthread_join(consumer[i], NULL);
     }
-
     pthread_join(producer, NULL);
-    pthread_join(consumer, NULL);
     //Final values of semapores
     sem_getvalue(&sSync, &finalSync);
     sem_getvalue(&sEmpty, &finalEmpty);
     sem_getvalue(&sFull, &finalFull);
     printf("sSync = %d, sEmpty = %d, sFull = %d\n", finalSync, finalEmpty, finalFull);
+    printf("Average Reponse Time = %f\n", calcAverage(dAverageResponseTime, NUMBER_OF_JOBS));
+    printf("Average Turnaround Time = %f\n", calcAverage(dAverageTurnAroundTime, NUMBER_OF_JOBS));
     return 0;
 }
